e08163300e40d7821497cbe1d0502cfbe344b04f
mchung-code <tkd.m.chung@gmail.com>
2021-12-23 22:59:34 -0500

    Solved day 2 problem

--- 2021/day2.py
+++ 2021/day2.py
@@ -12,6 +12,7 @@ merged_position_map.update(position_map_horizontal)
 
 def dive(movements: pd.DataFrame) -> float:
     """
+    Day 2 - Part 1
     Compute the final positions of where the submarine lands, given input movement instructions
 
     Args:
@@ -31,6 +32,38 @@ def dive(movements: pd.DataFrame) -> float:
     return x * y
 
 
+def dive_aim(movements: pd.DataFrame) -> float:
+    """
+    Day 2 - Part 2
+    Compute the final positions of where the submarine lands, given input movement instructions.  Now part 2 incorporates
+    aim.
+
+    Args:
+        movements: dataframe containing directions and quantity of units to move in each direction
+
+    Returns:
+        multiplication of the final horizontal position and depth
+        aim has been used to compute the depth
+    """
+    movements[['direction', 'amount']] = movements['value'].str.split(' ', 1, expand=True)
+    movements['amount'] = movements['amount'].astype(int)
+    movements['implied_dir'] = movements['direction'].map(merged_position_map)
+    movements['pos_update'] = movements['amount'] * movements['implied_dir']
+
+    aim_moves = movements.loc[movements['direction'].isin(position_map_vertical.keys())]
+    movements.loc[aim_moves.index, 'aim'] = aim_moves['pos_update'].cumsum()
+    movements['aim'].ffill(inplace=True)
+    movements.fillna(0, inplace=True)
+    movements['y_pos'] = movements['pos_update'] * movements['aim']
+
+    movements_final = movements.loc[movements['direction'].isin(position_map_horizontal.keys())]
+
+    x = movements_final['pos_update'].sum()
+    y = movements_final['y_pos'].sum()
+
+    return x * y
+
+
 class TestDive(unittest.TestCase):
     def test_dive_position(self):
         test_input = pd.DataFrame(['forward 5',
@@ -41,9 +74,18 @@ class TestDive(unittest.TestCase):
                                    'forward 2'], columns=['value'])
         self.assertEqual(150, dive(test_input))
 
+    def test_dive_aim_position(self):
+        test_input = pd.DataFrame(['forward 5',
+                                   'down 5',
+                                   'forward 8',
+                                   'up 3',
+                                   'down 8',
+                                   'forward 2'], columns=['value'])
+        self.assertEqual(900, dive_aim(test_input))
+
 
 if __name__ == '__main__':
     df = pd.read_csv(r'./mfs_inputs/day2.csv')
     # depths = list(df.values.flatten())
 
-    print(dive(df))
+    print(dive_aim(df))
