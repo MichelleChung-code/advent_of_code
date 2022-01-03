dcfdaa040ac6fc2cb69d4a7e9df849a37f8c4e0a
mchung-code <tkd.m.chung@gmail.com>
2022-01-02 20:26:04 -0500

    Add solution to part 2 of day4 problem

--- 2021/day4.py
+++ 2021/day4.py
@@ -15,6 +15,33 @@ def bingo_score(draw_order_ls, bingo_sheets_ls) -> float:
                 return bingo_sheets_ls[i].sum().sum() * elem
 
 
+def bingo_score_win_last(draw_order_ls, bingo_sheets_ls) -> float:
+    steps_to_win_counter = [False] * len(bingo_sheets_ls)
+    elem_to_win = [False] * len(bingo_sheets_ls)
+    for i in range(len(bingo_sheets_ls)):
+        steps_to_win_i = 0
+
+        for elem in draw_order_ls:
+            bingo_sheets_ls[i] = bingo_sheets_ls[i].replace(elem, np.NaN)
+            # check if there is either a row or a column that is all NaN
+            win_row_bool = bingo_sheets_ls[i].isnull().apply(lambda x: all(x), axis=1).any()
+            win_col_bool = bingo_sheets_ls[i].isnull().apply(lambda x: all(x), axis=0).any()
+            steps_to_win_i += 1
+
+            if win_row_bool or win_col_bool:
+                # there is a winner
+                steps_to_win_counter[i] = steps_to_win_i
+                elem_to_win[i] = elem
+                break
+            elif not win_row_bool and not win_col_bool and elem == draw_order_ls[-1]:  # the board just lost completely
+                steps_to_win_counter[i] = False
+                elem_to_win[i] = False
+
+    index_last_winner = steps_to_win_counter.index(max(steps_to_win_counter))
+
+    return bingo_sheets_ls[index_last_winner].sum().sum() * elem_to_win[index_last_winner]
+
+
 def read_text_file(file_name):
     with open(file_name, 'r') as f:
         arr = f.read().splitlines()
@@ -43,9 +70,9 @@ def read_text_file(file_name):
 if __name__ == '__main__':
     draw_order_ls, bingo_sheets_ls = read_text_file(r'./mfs_inputs/day4_part1_sample.txt')
     assert bingo_score(draw_order_ls, bingo_sheets_ls) == 4512
+    assert bingo_score_win_last(draw_order_ls, bingo_sheets_ls) == 1924
 
     # actual problem
     draw_order_ls, bingo_sheets_ls = read_text_file(r'./mfs_inputs/day4.txt')
     print(bingo_score(draw_order_ls, bingo_sheets_ls))
-
-
+    print(bingo_score_win_last(draw_order_ls, bingo_sheets_ls))
